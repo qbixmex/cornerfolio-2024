f7bd8417a09280ec75b15a83aaccfcdba09abeb3
Daniel González Briseño <qbixmex@gmail.com>
2024-02-27 13:32:29 -0800

    create_user_model_schema
    - followed ERD Diagram
    - UserType ( typescript )
    - Mongoose Schema

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,88 @@
+import mongoose, { Schema, Model } from 'mongoose';
+
+export type UserType = {
+  name: string;
+  email: string;
+  password: string;
+  type: 'student' | 'client' | 'admin';
+  jobTitle: string;
+  startDate: Date;
+  endDate: Date;
+  active: boolean;
+  course: string;
+  schedule: 'morning' | 'afternoon' | 'evening';
+  portfolios: Schema.Types.ObjectId[];
+  // TODO: License
+};
+
+type timestamps = {
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type UserModel = Model<UserType & timestamps>;
+
+const UserSchema = new Schema<UserType, UserModel>({
+  name: {
+    type: String,
+    required: [ true, 'Name is required' ],
+  },
+  email: {
+    type: String,
+    required: [ true, 'Email is required' ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [ true, 'Password is required' ],
+  },
+  type: {
+    type: String,
+    enum: [ 'student', 'client', 'admin' ],
+    default: 'student'
+  },
+  jobTitle: {
+    type: String,
+    default: 'example: Full Stack Developer',
+  },
+  startDate: {
+    type: Date,
+    required: [ true, 'Start Date is required' ],
+  },
+  endDate: {
+    type: Date,
+    required: [ true, 'End Date is required' ],
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  course: {
+    type: String,
+    default: 'example: Web development',
+  },
+  schedule: {
+    type: String,
+    enum: [ 'morning', 'afternoon', 'evening' ],
+  },
+  // TODO: Portfolio
+  // portfolios: [{
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'Portfolio',
+  //   required: [ true, 'Portfolio is required' ],
+  // }],
+}, {
+  timestamps: true,
+});
+
+UserSchema.set('toJSON', {
+  virtuals: true, //? convert _id to id
+  versionKey: false,
+  transform: function(doc, ret, options) {
+    delete ret._id;
+  },
+});
+
+const User = mongoose.model<UserType, UserModel>("User", UserSchema);
+
+export default User;
