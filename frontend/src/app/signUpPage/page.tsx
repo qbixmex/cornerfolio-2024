5985ec13ee0ca1f568fa2d886e9f736ac8ba527f
Rafael Massimo <rafael_massimo@hotmail.com>
2024-02-27 14:48:49 -0800

    create a version of the signUpPage with some styles

--- frontend/src/app/signUpPage/page.tsx
+++ frontend/src/app/signUpPage/page.tsx
@@ -1,97 +1,10 @@
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { SignUpForm } from "@/components/signUpForm";
 
-export default async function SignUpPage() {
-	enum Role {
-		Client = "client",
-		Student = "student",
-		Admin = "admin",
-	}
-
-	const getUserData = async (formData: FormData) => {
-		"use server";
-		const userName = formData.get("user_name");
-		const email = formData.get("email");
-		const password = formData.get("password");
-		const confirmPassword = formData.get("confirm_password");
-		const className = formData.get("class");
-		const jobTitle = formData.get("job");
-		const scheduleHour = formData.get("schedule");
-
-		if (password !== confirmPassword) {
-			throw new Error("Your password is not matching, please try again");
-		}
-
-		const newUser = {
-			name: userName,
-			email: email,
-			password: password,
-			type: Role.Student,
-			jobTitle: jobTitle,
-			startDate: new Date(),
-			active: true,
-			class: className,
-			scheduleHour: scheduleHour,
-		};
-
-		fetch(`http://localhost:3005`, {
-			method: "POST",
-			headers: {
-				"content-type": "application/json",
-			},
-			body: JSON.stringify(newUser),
-		}).catch((err) => {
-			console.log("error");
-		});
-		// redirect("/loginPage");
-	};
-
+export default function SignUpPage() {
 	return (
-		<div>
+		<div className="bg-white">
 			<div>
-				<form action={getUserData}>
-					<div>
-						<label>Name User:</label>
-						<input type="text" name="user_name" />
-					</div>
-
-					<div>
-						<label>Email:</label>
-						<input type="text" name="email" />
-					</div>
-
-					<div>
-						<label>Password:</label>
-						<input type="password" name="password" />
-					</div>
-
-					<div>
-						<label>Confirm Password:</label>
-						<input type="password" name="confirm_password" />
-					</div>
-
-					<div>
-						<label>What is your Job Title?</label>
-						<input type="text" name="job" />
-					</div>
-
-					<div>
-						<label>In which Class are you enrolled?</label>
-						<input type="text" name="class" />
-					</div>
-
-					<div>
-						<label>Select one of these schedule hour:</label>
-						<select name="schedule" id="">
-							<option value="">Select an hour</option>
-							<option value="morning">Morning</option>
-							<option value="evening">Afternoon</option>
-							<option value="night">Evening</option>
-						</select>
-					</div>
-
-					<button type="submit">Create Profile</button>
-				</form>
+				<SignUpForm />
 			</div>
 		</div>
 	);
---
+++ frontend/src/components/signUpForm.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { getUserData } from "@/lib/actions/actions";
+import { ChangeEvent, useState } from "react";
+
+export function SignUpForm() {
+	const [name, setName] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [confirmPassword, setConfirmPassword] = useState<string>("");
+	const [error, setError] = useState<string | null>(null);
+
+	const validateConfirmPassword = () => {
+		if (password !== confirmPassword) {
+			setError("Your passwords are not matching, please check again");
+		} else {
+			setError(null);
+		}
+	};
+
+	return (
+		<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 h-screen">
+			<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+				SignUp Page
+			</h2>
+			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+				<form className="space-y-6" action={getUserData}>
+					<div>
+						<label className="block text-sm font-medium leading-6 text-gray-900">Name User:</label>
+						<input
+							type="text"
+							name="user_name"
+							value={name}
+							onChange={(event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)}
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium leading-6 text-gray-900">Email:</label>
+						<input
+							type="text"
+							name="email"
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium leading-6 text-gray-900">Password:</label>
+						<input
+							type="password"
+							name="password"
+							value={password}
+							onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium leading-6 text-gray-900">
+							Confirm Password:
+						</label>
+						<input
+							type="password"
+							name="confirm_password"
+							value={confirmPassword}
+							onChange={(event: ChangeEvent<HTMLInputElement>) =>
+								setConfirmPassword(event.target.value)
+							}
+							onBlur={validateConfirmPassword}
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+						/>
+						{error && (
+							<div
+								className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+								role="alert"
+							>
+								<p className="font-bold">Error:</p>
+								<p className="text-sm">{error}</p>
+							</div>
+						)}
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium leading-6 text-gray-900">
+							What is your Job Title?
+						</label>
+						<input
+							type="text"
+							name="job"
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium leading-6 text-gray-900">
+							In which Class are you enrolled?
+						</label>
+						<input
+							type="text"
+							name="class"
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium leading-6 text-gray-900">
+							Select one of these schedule hour:
+						</label>
+						<select
+							name="schedule"
+							id=""
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+						>
+							<option value="">Select an hour</option>
+							<option value="morning">Morning</option>
+							<option value="evening">Afternoon</option>
+							<option value="night">Evening</option>
+						</select>
+					</div>
+
+					<button
+						type="submit"
+						disabled={!!error}
+						className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>
+						Create Profile
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+}
