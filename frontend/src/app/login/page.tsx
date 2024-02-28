97082e90c6ff07033e7b83731283eb15c448baf9
Daniel González Briseño <qbixmex@gmail.com>
2024-02-28 08:44:17 -0800

    some-improvements

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,26 +1,36 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import data from "../../../user.json";
+import { useState, ChangeEvent } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import data from '../../../user.json';
+
+type FormData = {
+    email: string;
+    password: string;
+};
+
+const FORM_DATA = {
+    email: "",
+    password: "",
+};
 
 const Login = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+    const [formData, setFormData] = useState<FormData>(FORM_DATA);
+    const { email, password } = formData;
+
+    const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
     const [error, setError] = useState<string | null>();
 
     const [showPassword, setShowPassword] = useState<boolean>(false);
 
     const router = useRouter();
-
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    };
-
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
 
     const togglePasswordVisibility = () => {
         if (!showPassword) {
@@ -30,8 +40,8 @@ const Login = () => {
         }
     };
 
-    const checkLoginInfo = (e: React.FormEvent) => {
-        e.preventDefault();
+    const checkLoginInfo = (event: React.FormEvent) => {
+        event.preventDefault();
 
         const matchedUser = data.find(
             (user) => user.email === email && user.password === password
@@ -73,9 +83,8 @@ const Login = () => {
                                 name="email"
                                 type="email"
                                 autoComplete="off"
-                                placeholder="Email address"
                                 value={email}
-                                onChange={handleEmailChange}
+                                onChange={onInputChange}
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                             />
@@ -88,14 +97,14 @@ const Login = () => {
                                 htmlFor="password"
                                 className="block text-sm font-medium leading-6 text-gray-900"
                             >
-                                password
+                                Password
                             </label>
                             <div className="text-sm">
                                 <Link
                                     href={"/"}
                                     className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                                 >
-                                    Forgot password?
+                                    Forgot password ?
                                 </Link>
                             </div>
                         </div>
@@ -104,18 +113,20 @@ const Login = () => {
                                 id="password"
                                 name="password"
                                 type={showPassword ? "text" : "password"}
-                                placeholder="password"
                                 value={password}
-                                onChange={handlePasswordChange}
+                                onChange={onInputChange}
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                             />
-                            <button
-                                type="button"
-                                onClick={togglePasswordVisibility}
-                            >
-                                {showPassword ? "Hide" : "Show"}
-                            </button>
+                            <div className="flex justify-end">
+                                <button
+                                    type="button"
+                                    onClick={togglePasswordVisibility}
+                                    className="mt-2 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                                >
+                                    {showPassword ? "hide" : "show"}
+                                </button>
+                            </div>
                         </div>
                     </div>
 
@@ -124,7 +135,7 @@ const Login = () => {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Log in
+                            login
                         </button>
                     </div>
                 </form>
@@ -132,10 +143,10 @@ const Login = () => {
                 <p className="mt-10 text-center text-sm text-gray-500">
                     Not a member?
                     <Link
-                        href={"/signup"}
+                        href={"/register"}
                         className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ml-2"
                     >
-                        Sign up
+                        Register
                     </Link>
                 </p>
             </div>
