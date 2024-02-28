08456c11fe5b525257a441b21a40fc65fbc02649
Rafael Massimo <rafael_massimo@hotmail.com>
2024-02-27 16:30:17 -0800

    fix the title of the sign up page

--- frontend/src/components/signUpForm.tsx
+++ frontend/src/components/signUpForm.tsx
@@ -20,7 +20,7 @@ export function SignUpForm() {
 	return (
 		<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 h-screen">
 			<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-				SignUp Page
+				Register New Account
 			</h2>
 			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 				<form className="space-y-6" action={getUserData}>
