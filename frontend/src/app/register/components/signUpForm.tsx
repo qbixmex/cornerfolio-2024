7a7e6bcbb7273c182df88861f6c272cbb55820cd
Daniel González Briseño <qbixmex@gmail.com>
2024-03-05 10:02:18 -0800

    admin-folder-re-structure

--- frontend/src/app/register/components/signUpForm.tsx
+++ frontend/src/app/register/components/signUpForm.tsx
@@ -50,8 +50,8 @@ export function SignUpForm() {
 	};
 
 	return (
-		<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 h-screen">
-			<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+		<div className="flex min-h-full flex-col justify-center px-6 lg:px-8 h-screen">
+			<h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
 				Register New Account
 			</h2>
 			{errorMessage && <ErrorMessage message={errorMessage} />}
