3987dc7b8ef00ea72d64ab6cb14a5fdb2698d100
Daniel González Briseño <qbixmex@gmail.com>
2024-03-07 08:37:03 -0800

    errors-fixes

---
+++ frontend/src/app/admin/portfolios/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { MdAdd } from 'react-icons/md';
+import { getPortfolio } from '@/portfolios/actions/portfolio.action';
+import { TemplateHeader, TemplateFooter } from '@/portfolios/components';
+
+type Props = {
+    params: { id: string };
+    searchParams: {};
+};
+
+const EditPortfolioPage: FC<Props> = async ({ params }) => {
+    const portfolio = await getPortfolio(params.id);
+
+    return (
+        <main className="ml-[52px] mt-[55px] text-2xl font-bold">
+            <TemplateHeader header={portfolio.header} />
+            {portfolio.sections.length !== 0 && (
+                <section className="mx-[80px] mt-10 flex flex-col items-center gap-3">
+                    <section className="bg-orange-500 rounded text-white w-fit p-5">
+                        No section created yet
+                    </section>
+                    <button className="bg-slate-600 p-2 rounded text-white">
+                        <MdAdd size={50} />
+                    </button>
+                </section>
+            )}
+            <TemplateFooter footer={portfolio.footer} />
+        </main>
+    );
+};
+
+export default EditPortfolioPage;
---
+++ frontend/src/portfolios/actions/portfolio.action.ts
@@ -0,0 +1,6 @@
+'use server';
+
+export const getPortfolio = async (id: string) => {
+    const response = await fetch(`http://localhost:4000/api/portfolio/${id}`);
+    return response.json();
+};
