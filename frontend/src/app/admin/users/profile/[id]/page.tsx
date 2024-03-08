6e0a895595568a55c7efce13a5d7638d9ac9c4b1
Daniel González Briseño <qbixmex@gmail.com>
2024-03-08 09:10:55 -0800

    user-profile-update

--- frontend/src/app/admin/users/profile/[id]/page.tsx
+++ frontend/src/app/admin/users/profile/[id]/page.tsx
@@ -1,15 +1,16 @@
-import ProfileBody from "@/components/profile";
-import { getUser } from "@/users/actions/user.actions";
 import { FC } from "react";
+import { getUser } from "@/users/actions/user.actions";
+import { ProfileBody } from "@/users/components";
 
 type Props = {
-    params: { id: string };
+	params: { id: string };
 };
 
 const ProfilePage: FC<Props> = async ({ params }) => {
-    const userInfo = await getUser(params.id);
-
-    return <ProfileBody user={userInfo.user} />;
+	const userInfo = await getUser(params.id);
+	return (
+		<ProfileBody user={userInfo.user} />
+	);
 };
 
 export default ProfilePage;
