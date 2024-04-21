c5cd243465b5bd8ab7bd8de423b97ccf90a3af6c
Daniel González Briseño <qbixmex@gmail.com>
2024-04-20 19:47:57 -0700

    Live Preview URL

--- frontend/src/portfolios/components/manageStatus.tsx
+++ frontend/src/portfolios/components/manageStatus.tsx
@@ -83,7 +83,7 @@ const ManageStatus: React.FC<Readonly<Props>> = ({ portfolioId, tinyUrlId, statu
       )}
 
       <Link
-        href={`${HOST}/${tinyUrlId}`}
+        href={`/${tinyUrlId}`}
         target="blank"
         className="rounded-md bg-blue-600 hover:bg-blue-500 border m-2 mr-20 px-4 py-2 justify-between text-white text-base transition-colors"
         title="Live Portfolio Preview"
